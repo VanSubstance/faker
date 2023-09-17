b1d0db1e30e2175d706d87250845330bc0980068
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-09-17 15:23:01 +0900

    Feat: Move Camera via Drag
    #6
    
    + 카메라 45도 고정
    + 드래그 앤 드롭으로 카메라 이동

---
+++ ProjectFaker/Assets/Scripts/Globals/MainCameraMover.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Faker.Globals
+{
+	public class MainCameraMover : MonoBehaviour
+	{
+		private Vector3? posDragStart, posDragIng;
+		private Vector3 dirMove;
+
+		private void Update()
+		{
+			if (Input.GetMouseButtonDown(0)) {
+				posDragStart = Camera.main.WorldToViewportPoint(Input.mousePosition);
+			}
+
+			if (posDragStart != null) {
+				if (Input.GetMouseButton(0)) {
+					posDragIng = Camera.main.WorldToViewportPoint(Input.mousePosition);
+					Move();
+				} else {
+					posDragStart = null;
+					posDragIng = null;
+				}
+			}
+		}
+
+		private void Move()
+		{
+			dirMove = (Vector3)(posDragIng - posDragStart);
+			dirMove.y = 0;
+			dirMove.z = -dirMove.z;
+			dirMove /= 10;
+			float curLeng;
+			if ((curLeng = dirMove.magnitude) > 5f) {
+				dirMove *= 5f / curLeng;
+			}
+			transform.Translate(dirMove * 2 * Time.deltaTime);
+		}
+	}
+}
