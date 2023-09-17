ef79e2865490a19eb83a6c4939ed895efbd08c7c
VanSubstance <50601968+VanSubstance@users.noreply.github.com>
2023-09-17 18:46:52 +0900

    Feat: Footer UI
    #7
    
    + Canvas 기반 컨텐츠 이동을 위한 기본 푸터 구성
    + 노치 부분 제외를 위한 SafeArea 구성

---
+++ ProjectFaker/Assets/Scripts/UI/ButtonController.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Faker.UI
+{
+	[RequireComponent(typeof(Button))]
+	public class ButtonController : MonoBehaviour
+	{
+		[SerializeField]
+		private string text;
+		[SerializeField]
+		private Sprite sprite;
+
+		[SerializeField]
+		private TextMeshProUGUI tmp;
+		[SerializeField]
+		private Image img;
+
+		private void Awake()
+		{
+			tmp.text = text;
+			img.sprite = sprite;
+		}
+	}
+}
