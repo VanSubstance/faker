8318fefc283b6deb2a2b8a0f49720c583458ac9b
Gangmin Park <wjdrmf314@gmail.com>
2023-11-02 19:22:47 +0900

    Feat: Scroll View for item on class
    + 기존 scrollViewContent-> scrollViewContent_class로 변경
    + scrlooViewContent_item 추가
    + button 눌렀을 때 새로운 scrollview나타나도록 추가

--- ProjectFaker/Assets/Scripts/UI/ButtonManager.cs
+++ ProjectFaker/Assets/Scripts/UI/ButtonManager.cs
@@ -7,18 +7,35 @@ public class ButtonManager : MonoBehaviour
 {
 
     [SerializeField]
-    private Transform scrollViewContent;
+    private Transform scrollViewContent_class;
+    [SerializeField]
+    private Transform scrollViewContent_item;
     [SerializeField]
     private Transform buttonTemplate;
 
-    public int btnCount = 10;
+    public int classBtnCount = 4;
+    public int itemBtnCount=5;
     
     void Start()
     {
-      for (int i = 0; i < btnCount; i++){
-        Instantiate(buttonTemplate,scrollViewContent.transform);
+      ActiveScrollViewContent_class();
+    }
+
+    void ActiveScrollViewContent_class()
+    {
+      for (int i = 0; i < classBtnCount; i++){
+        Transform button = Instantiate(buttonTemplate,scrollViewContent_class.transform);
+        int btnId = i;
+        button.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => ActiveScrollViewContent_item(btnId));
       }
-        
+    }
+
+    void ActiveScrollViewContent_item(int classId)
+    { 
+      scrollViewContent_class.gameObject.SetActive(false);
+      for (int i = 0; i < itemBtnCount; i++){
+        Instantiate(buttonTemplate,scrollViewContent_item);
+      }
     }
 }
 
