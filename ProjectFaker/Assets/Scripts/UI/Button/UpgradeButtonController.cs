215c37b46fb500076efd4e1001764765fc5ea957
Junmin Kim <43731900+wnsals1346@users.noreply.github.com>
2023-11-10 15:41:31 +0900

    feat : 피드백 반영

--- ProjectFaker/Assets/Scripts/UI/Button/UpgradeButtonController.cs
+++ ProjectFaker/Assets/Scripts/UI/Button/UpgradeButtonController.cs
@@ -1,47 +1,43 @@
-﻿using TMPro;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.UI;
-
-namespace Faker.UI.Button
+﻿namespace Faker.UI.Button
 {
 	[RequireComponent(typeof(UnityEngine.UI.Button))]
 	public class UpgradeButtonController : MonoBehaviour
 	{
-	    [SerializeField]
+		[SerializeField]
 		private Sprite sprite;
 		[SerializeField]
 		private string text;
 		private int level = 0;
 		[SerializeField]
-		private TextMeshProUGUI statName;
-		[SerializeField]
-		private TextMeshProUGUI statValue;
+		private TextMeshProUGUI statName, statValue;
 		[SerializeField]
 		private Image img;
 
-		private UnityEngine.UI.Button btn {
-		get {
-		return GetComponent<UnityEngine.UI.Button>();
-		}
+
+		private UnityEngine.UI.Button btn
+		{
+			get
+			{
+				return GetComponent<UnityEngine.UI.Button>();
+			}
 		}
 
 		protected void Awake()
 		{
-		statName.text = text;
-		statValue.text = "현재 : " + level;
-		img.sprite = sprite;
+			statName.text = text;
+			statValue.text = $"현재 : {level}";
+			img.sprite = sprite;
 		}
 
 		public void AddClickEvent(UnityAction actionToAdd)
 		{
-		btn.onClick.AddListener(actionToAdd);
+			btn.onClick.AddListener(actionToAdd);
 		}
 
 		public void Onclick()
 		{
-		level += 1;
-		statValue.text = "현재 : " + level;
+			level += 1;
+			statValue.text = $"현재 : {level}";
 		}
 	}
 }
